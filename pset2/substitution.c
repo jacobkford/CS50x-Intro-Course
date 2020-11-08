560128cfe453c88cad9a557af86ffe2fbe429b47
Jay <68663887+jacobkford@users.noreply.github.com>
2020-11-08 00:58:13 +0000

    Substitution cipher

--- pset2/substitution.c
+++ pset2/substitution.c
@@ -1,59 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <cs50.h>
 #include <ctype.h>
 #include <string.h>
 
 int main(int argc, string argv[])
 {
-    int argl = 0;
     int upper_key[26];
     int lower_key[26];
 
-    // Checking if the user provides a vaild number of inputs.
-    if (argc == 2)
-    {
-        // Calculating how long the 2nd input is.
-        argl = strlen(argv[1]);
-        if (argl == 26)
-        {
-            for (int i = 0; i < 26; i++)
-            {
-                // Checking if the all the characters in the 2nd input are digits, returns message if 1 or more are not digits.
-                if (!isalpha(argv[1][i]))
-                {
-                    printf("Usage: ./substitution key\n");
-                    return 1;
-                }
-                else
-                {
-                    // Checking for duplicate characters.
-                    for (int d = 0; d < i; d++)
-                    {
-                        if (argv[1][i] == argv[1][d])
-                        {
-                            printf("Key must not contain repeated characters.\n");
-                            return 1;
-                        }
-                    }
-                    // Creating an uppercase & lowercase version of the key.
-                    upper_key[i] = toupper(argv[1][i]);
-                    lower_key[i] = tolower(argv[1][i]);
-                }
-            }
-            // Stating the variable for the cipher key.
-        }
-        else
-        {
-            printf("Usage: ./substitution key (26 Characters)\n");
-            return 1;
-        }
-    }
-    // Returns message if an incorrect amount of inputs is executed.
-    else
+    // Checking if the user provides a vaild number of inputs, and if the 2nd input is 26 digits in length.
+    if (argc != 2 || strlen(argv[1]) != 26)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
+
+    for (int i = 0; i < 26; i++)
+    {
+        // Checking if the all the characters in the 2nd input are digits, returns message if 1 or more are not digits.
+        if (isalpha(argv[1][i]))
+        {
+            // Creating an uppercase & lowercase version of the key.
+            upper_key[i] = toupper(argv[1][i]);
+            lower_key[i] = tolower(argv[1][i]);
+
+            // Checking for duplicate characters.
+            for (int d = 0; d < i; d++)
+            {
+                if (argv[1][i] == argv[1][d])
+                {
+                    printf("Key must not contain repeated characters.\n");
+                    return 1;
+                }
+            }
+        }
+        else
+        {
+            printf("Usage: ./substitution key\n");
+            return 1;
+        }
+    }
+
     // Asking the user for their text input.
     string input = get_string("plaintext: ");
     int text_length = strlen(input);
@@ -99,5 +87,6 @@ int main(int argc, string argv[])
     }
     // Prints a new line once all characters have been sorted & ciphered.
     printf("\n");
+    // Ends the program.
     return 0;
 }
