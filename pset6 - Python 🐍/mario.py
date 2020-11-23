079306c2b19ec872e14f51688a745ce1a2d3050d
Jay <68663887+jacobkford@users.noreply.github.com>
2020-11-23 22:42:57 +0000

    pyramid

--- pset6 - Python 🐍/mario.py
+++ pset6 - Python 🐍/mario.py
@@ -2,7 +2,7 @@ from cs50 import get_int
 
 # Asks user for input the height value, it will repeat until answer is between numbers 1 - 8
 h = 0
-while (h > 8 or h < 1):
+while h > 8 or h < 1:
     h = get_int("Height: ")
 
 # Prints the pyramid of #'s
