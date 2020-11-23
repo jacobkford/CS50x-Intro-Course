d2a3655aec5bffa483fe81bafa52ddf886bc2aab
Jay <68663887+jacobkford@users.noreply.github.com>
2020-11-23 22:41:45 +0000

    Greedy Algorithm

--- pset6 - Python 🐍/cash.py
+++ pset6 - Python 🐍/cash.py
@@ -2,7 +2,7 @@ from cs50 import get_float
 
 # Asks user for input the change value, it will repeat the question if the answer isn't a positive number
 dollars = 0
-while (dollars <= 0):
+while dollars <= 0:
     dollars = get_float("Change owed: ")
 
 # Converting currency from Dollars to Cents
@@ -10,17 +10,17 @@ cents = round(dollars * 100)
 coins = 0
 
 # Counts through the coins until the change owed is equal to 0
-while (cents > 0):
+while cents > 0:
     # Quarter
-    if (cents >= 25):
+    if cents >= 25:
         cents -= 25
         coins += 1
     # Dime
-    elif (cents >= 10):
+    elif cents >= 10:
         cents -= 10
         coins += 1
     # Nickel
-    elif (cents >= 5):
+    elif cents >= 5:
         cents -= 5
         coins += 1
     else:
