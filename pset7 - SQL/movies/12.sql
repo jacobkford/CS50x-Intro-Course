55086b9e0878c7c7976900a5436c15440147424c
Jay <68663887+jacobkford@users.noreply.github.com>
2020-12-03 00:49:02 +0000

    Movies

---
+++ pset7 - SQL/movies/12.sql
@@ -0,0 +1,11 @@
+SELECT title
+FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE name = "Helena Bonham Carter"
+INTERSECT
+SELECT title
+FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE name = "Johnny Depp";
